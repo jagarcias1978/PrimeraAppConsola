fa91a6af26a8963ed5747b0f46967543d1626bc0
Jose Alberto <jagarcias@cifpvirgendegracia.com>
2020-09-15 10:14:00 +0200

    Actualización al castellano

--- PrimeraAppConsola/Program.cs
+++ PrimeraAppConsola/Program.cs
@@ -6,7 +6,8 @@ namespace PrimeraAppConsola
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Actualización del msg en castellano
+            Console.WriteLine("Hola mundo!");
         }
     }
 }
