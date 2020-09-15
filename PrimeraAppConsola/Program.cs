a16a914d666f62ca16a6478ce206be0553dd2672
Jose Alberto <jagarcias@cifpvirgendegracia.com>
2020-09-15 10:05:07 +0200

    Agregar archivos de proyecto.

---
+++ PrimeraAppConsola/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimeraAppConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
